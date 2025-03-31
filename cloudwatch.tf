8e58aaa8d611fb196010f0331f3bc656f7130952
bhuvan-dv <damavenkateshraj.b@northeastern.edu>
2025-03-31 16:35:54 -0400

    added cw instance profile

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -3,3 +3,8 @@ resource "aws_iam_role_policy_attachment" "attach_cloudwatch_policy" {
   role       = aws_iam_role.s3_access_role.name
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
 }
+
+resource "aws_iam_instance_profile" "cloudwatch_agent_profile" {
+  name = "cloudwatch_agent_instance_profile"
+  role = aws_iam_role.s3_access_role.name
+}
